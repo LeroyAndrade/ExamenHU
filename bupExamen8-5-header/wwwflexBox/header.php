16f515c327e3355f91f8baeab15754f928ac1869
LeroyAndrade <30168@ma-web.nl>
2022-05-08 05:14:49 +0200

    +header
    +header

---
+++ bupExamen8-5-header/wwwflexBox/header.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+ <html lang="nl">
+ <head>
+  <meta charset="utf-8">
+  <meta name="description" content="HW">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="keywords" content=" ">
+  <meta name="author" content="Leroy Andrade">
+
+  <title>Hogeschool Utrecht</title>
+  <meta name="author" content="Hier komt jouw naam">
+  <link rel="stylesheet" type="text/css" href="./opmaak/header.css?">
+ </head>
+
+ <body lang="nl">
+
+ <div id="headerContainer">
+  <br>
+  <section class="top">
+
+   <!--          img1-->
+
+   <div id="placeholderImage"><img src="./img/main/placeholderImage.svg" alt="test"></div>
+   <!--      <span><img src="https://via.placeholder.com/70x46" alt="test"></span>-->
+
+   <!--leeg 2/3  -->
+      <div id="menuAchtergrond" class="menuAchtergrond verbergen hideForDesktop">
+          <ul id="geopendeMenuItems" class="geopendeMenuItems verbergen hideForDesktop">
+              <li><a href= "#" class="navigationItems nav__item hideForDesktop"> <span class="nav__link">    Home       </span> </a></li>
+              <li><a href= "#" class="navigationItems nav__item hideForDesktop"> <span class="nav__link">    Overzicht  </span> </a></li>
+              <li><a href= "#" class="navigationItems nav__item hideForDesktop"> <span class="nav__link">    Over       </span> </a></li>
+              <li><a href= "#" class="navigationItems nav__item hideForDesktop"> <span class="nav__link">    Contact    </span> </a></li>
+              <li> <a href= "#" class="navigationItems nav__item hideForDesktop"> <span class="nav__link">    <img src="./img/main/persoon-icoon.svg" alt="">    </span> </a></li>
+
+          </ul>
+      </div>
+<!---->
+      <a href= "#" class="navigationItems nav__item hideForMobile"> <span class="nav__link">    Home       </span> </a>
+      <a href= "#" class="navigationItems nav__item hideForMobile"> <span class="nav__link">    Overzicht  </span> </a>
+      <a href= "#" class="navigationItems nav__item hideForMobile"> <span class="nav__link">    Over       </span> </a>
+      <a href= "#" class="navigationItems nav__item hideForMobile"> <span class="nav__link">    Contact    </span> </a>
+      <a href= "#" class="navigationItems nav__item hideForMobile"> <span class="nav__link">    <img src="./img/main/persoon-icoon.svg" alt="">    </span> </a>
+
+
+      <!-- hamburger button -->
+      <!-- hamburger button -->
+
+      <div>
+          <div class="nav__knop">
+              <svg xmlns="http://www.w3.org/2000/svg" class="hamburgerButton nav__knop fas hamburger_button" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M4 6h16M4 12h16M4 18h16" />
+              </svg>
+
+              <div class="fas sluitKnop verbergen"> <!--&#215;-->
+                  <img src="./img/main/kruis.svg" alt="">
+              </div>
+          </div>
+      </div>
+
+
+  </section>
+
+  <hr>
+ </div>
+
+
+ <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br>
+ <div class="del">
+     <div>a</div>
+     <div>a</div>
+     <div>a</div>
+ </div>
+
+ <script src="./js/javaScript.js"></script>
+ </body>
+</html>
